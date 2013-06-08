fe6ec974a5e7d10e8e630319b823b70daddce43c
joaogl <joaoguerralourenco@hotmail.com>
2013-06-08 17:07:10 +0100

    ProgramInfo clean up.

--- GeoQuest/src/me/joaogl/data/ProgramInfo.java
+++ GeoQuest/src/me/joaogl/data/ProgramInfo.java
@@ -1,9 +1,12 @@
 package me.joaogl.data;
 
 public class ProgramInfo {
-
+	
+	// Debugger
 	public static boolean debugging = false;
 	public static int debugquestions = 1;
+	
+	// Credits
 	public static String Author = "João Lourenço";
 	public static String Group = "André Gomes, Hugo Barroca, João Lourenço.";
 	public static String Subject = "Geografia";
@@ -11,11 +14,12 @@ public class ProgramInfo {
 	public static String Class = "9º 1ª";
 	public static String Version = "V1.1";
 	public static String Nome = "Jogo da Fome - Geografia 9º1ª";
-	public static boolean open = false;
-	public static boolean close = false;
 	
 	// Timers
 	public static int questionTime = 13;
 	public static int totalTime = 15;
 	
+	// Program Stuff DONT TOUCH THIS	
+	public static boolean open = false;
+	public static boolean close = false;
 }
